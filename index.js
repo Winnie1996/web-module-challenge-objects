1dfe60ea5c8077b67974bbd831be09b359ae506c
HungT <64987549+Winnie1996@users.noreply.github.com>
2020-07-16 16:49:24 -0700

    Finished Assignment

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 ///////////////Menu Items (MVP)///////////////////
 
-const latte = {name: "Cafe Latte", price: 4, category: "Drinks"};
-const burger = {name: "Burger", price: 18, category: "Lunch"};
-const breakfastBurrito = {name: "Breakfast Burrito", price: 16, category:"Breakfast"};
+const latte = { name: "Cafe Latte", price: 4, category: "Drinks" };
+const burger = { name: "Burger", price: 18, category: "Lunch" };
+const breakfastBurrito = { name: "Breakfast Burrito", price: 16, category: "Breakfast" };
 
 /* Task 1a: write a function to return more menu items with the same format as the items above. */
 
-function createMenuItem(name, cost, category){
-    /* Code here */
+function createMenuItem(name, price, category) {
+  return { name, price, category };
+
 }
-
+console.log(createMenuItem('tacos', 8, 'Lunch'))
+console.log(createMenuItem('pizza', 10, 'dinner'))
+console.log(createMenuItem('cheesecake', 4, 'Dessert'))
 /* Task 1b: use your function to create 3 more menu items. You may add any items to the menu that you'd like */
-
+console.log(createMenuItem('sandwich', 6, 'Lunch'))
+console.log(createMenuItem('lobster', 10, 'dinner'))
+console.log(createMenuItem('oreo', 7, 'Dessert'))
 
 
 /* Task 2: You're having a lunch special! 25% off for teachers and students, 10% off for everyone else. Add a method to your burger object that automatically calculates price given a string as a parameter. 
@@ -23,28 +28,72 @@ Your method should accept:
 and should return a number. 
 
 For example, burger.discount("teacher") would return 13.5 and burger.discount("public") would return 16.2*/
+function discount(patron) {
+  let discount = 0;
+
+  if (patron === 'teacher' || patron === 'student') {
+    discount = this.price * 0.25;
+  }
+  else if (patron === 'public') {
+    discount = this.price * 0.10;
+  }
+  let finalPrice = this.price - discount;
+  return finalPrice;
+}
+
+burger.discount = discount;
+
+console.log(burger.discount('public'));
 
 
 
 ///////////////Reviews (MVP)///////////////////
 
-const reviews = [{name: "Daniela", rating: 5, feedback:"Beautiful atmosphere and wonderful vegan options!"},
-    {name: "Jack", rating: 3, feedback:"A little too hipster for my taste, but the burger was decent, if overpriced"},
-    {name: "Miranda", rating: 4, feedback:"fun trivia and cool vibes"},
-    {name: "Wen", rating: 4.5, feedback:"I don't leave my house often, but when I do, it's for this place. Highly reccomend."},
-    {name: "Brett", rating: 3, feedback: "great selection of snacks and a nice cafe area to get work done during the day."},
-    {name: "Julius", rating: 2, feedback: "I was largely unimpressed by this venue. Nothing special on the menu and too expensive. The atmosphere is polarizing, and not for me, but I think some would like it." },
-    {name:"Lauren", rating: 4, feedback: "Absolutely love that they have karaoke Fridays! Food and drink selection is okay."},
-    {name:"Reyna", rating: 3.5, feedback: ""},
+const reviews = [{ name: "Daniela", rating: 5, feedback: "Beautiful atmosphere and wonderful vegan options!" },
+{ name: "Jack", rating: 3, feedback: "A little too hipster for my taste, but the burger was decent, if overpriced" },
+{ name: "Miranda", rating: 4, feedback: "fun trivia and cool vibes" },
+{ name: "Wen", rating: 4.5, feedback: "I don't leave my house often, but when I do, it's for this place. Highly reccomend." },
+{ name: "Brett", rating: 3, feedback: "great selection of snacks and a nice cafe area to get work done during the day." },
+{ name: "Julius", rating: 2, feedback: "I was largely unimpressed by this venue. Nothing special on the menu and too expensive. The atmosphere is polarizing, and not for me, but I think some would like it." },
+{ name: "Lauren", rating: 4, feedback: "Absolutely love that they have karaoke Fridays! Food and drink selection is okay." },
+{ name: "Reyna", rating: 3.5, feedback: "" },
 ]
 
 /* Task 3: Console.log just Julius' feedback */
 
+for (let i = 0; i < reviews.length; i++)
+  if (reviews[i].name === 'Julius') {
+    console.log(reviews[i].feedback)
+
+  }
+
 
 /* Task 4: Add a new rating with your (fictitious) opinions of the restaurant in the same format as the reviews above. */
+function addReview(tempname, myrating, badfeedback) {
+  reviews.push({ name: tempname, rating: myrating, feedback: badfeedback })
+
+}
+addReview('Win', '10', 'it was ok I guess')
+console.log(reviews)
 
 
-/* Task 5: Add the following feedback to Reyna's rating - "this place is chill with really cool people, great for getting work done on weekdays"
+
+/* Task 5: Add the following feedback to Reyna's rating - "this place is chill with really cool people, great for getting work done on weekdays"*/
+function changeFeedback(insertname, insertfeedback) {
+  for (let i = 0; i < reviews.length; i++) {
+    if (reviews[i].name === insertname) {
+      reviews[i].feedback = insertfeedback
+    }
+  }
+}
+changeFeedback('Reyna', 'this place is chill with really cool people, great for getting work done on weekdays')
+console.log(reviews)
+
+
+
+
+
+
 
 /*  Task 6: Write a function to return a review based on the index of the review in the array.
 
@@ -58,12 +107,20 @@ and should return a string in the format `{name} gave the restaurant a {rating},
  * For example, if getReviewByIndex is invoked with reviews and the number 0
  * it will return `Daniela gave the restaurant a 5 star review and their feedback was: Beautiful atmosphere and wonderful vegan options!`
 */
-function getReviewByIndex(reviews, index) {
-    /* code here */
-  }
-  
+function getReviewByIndex(reviews, index){
+ let yelpname = reviews[index].name
+ let yelpreview = reviews[index].rating
+ let yelpfeedback = reviews[index].feedback
+  return(yelpname + ' gave the restaurant a ' +  yelpreview + ' and their feedback was ' + yelpfeedback)
+
+}
+
+console.log(getReviewByIndex(reviews,3))
+
+
 
 /* Task 7: Write a function to get information about the most recent review called `getLastReview`
+
 
 getLastReview should accept:
   (1) an array of objects 
@@ -72,9 +129,14 @@ and should return a string in the format `name} gave the restaurant a {rating}, 
 
 For example, if getLastReview is invoked passing the reviews array it will return `Reyna gave the restaurant a 3.5 star review and their feedback was: "this place is chill with really cool people, great for getting work done on weekdays"`.
 */
-function getLastReview(/* code here */) {
-    /* code here */
-  } 
+function getLastReview(reviews) {
+let lastreview = reviews[reviews.length - 1] 
+return (lastreview.name + ' gave the restaurant a ' +  lastreview.review + ' and their feedback was ' + lastreview.feedback)
+}
+
+console.log(getLastReview(reviews));
+
+yay
 
 
 ///////////////🍔☕️🍽 STRETCH🍔☕️🍽////////////////////
@@ -91,11 +153,11 @@ function getLastReview(/* code here */) {
     {name:"Lauren", rating: 4, feedback: "Absolutely love that they have karaoke Fridays! Food and drink selection is okay."}]
 */
 
- function getReviewByRating(/* code here */) {
-    /* code here */
-  }
+function getReviewByRating(/* code here */) {
+  /* code here */
+}
 
-  
+
 /** STRETCH 2: Write a function called 'getLongestReview' that returns an array containing all reviews longer than 15 words. 
   
 Your function should accept: 
@@ -109,10 +171,10 @@ and should return an array of objects.
     {name: "Brett", rating: 3, feedback: "great selection of snacks and a nice cafe area to get work done during the day."},
     {name: "Julius", rating: 2, feedback: "I was largely unimpressed by this venue. Nothing special on the menu and too expensive. The atmosphere is polarizing, and not for me, but I think some would like it." }]
 */
-  function getLongReviews(/* code here */) {
-    /* code here */
-  }
-  
+function getLongReviews(/* code here */) {
+  /* code here */
+}
+
 
 /* STRETCH 3:  This challenge is not related to the data above! 
 
@@ -133,6 +195,6 @@ The returned object should have the following characteristics:
 
 
 function carMaker(/* code here */) {
-    /* code here */
-    
+  /* code here */
+
 }
